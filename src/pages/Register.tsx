ac06113b08f83f15589add98f9376f9ac3f3b783
pardo133 <pardo133@hotmail.com>
2026-05-05 17:24:39 +0200

    registro

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -21,11 +21,17 @@ const Register = () => {
     <div style={{ padding: '2rem' }}>
       <h2>Crear Cuenta</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register('username')} placeholder="Nombre de usuario" required />
-        <br /><br />
-        <input {...register('email')} type="email" placeholder="Email" required />
-        <br /><br />
-        <input {...register('password')} type="password" placeholder="Contraseña" required />
+       
+<input {...register('nombre')} placeholder="Nombre" required />
+
+
+<input {...register('apellido')} placeholder="Apellido" required />
+
+
+<input {...register('correo')} type="email" placeholder="Correo electrónico" required />
+
+
+<input {...register('password')} type="password" placeholder="Contraseña" required />
         <br /><br />
         <button type="submit">Registrarse</button>
       </form>
